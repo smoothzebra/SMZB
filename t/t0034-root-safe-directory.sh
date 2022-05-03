7d3d8252df1da78e7be3755552e8291ed16fbfa6
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2022-05-02 23:54:40 -0700

    t: document regression git safe.directory when using sudo
    Originally reported after release of v2.35.2 (and other maint branches)
    for CVE-2022-24765 and blocking otherwise harmless commands that were
    done using sudo in a repository that was owned by the user.
    
    Add a new test script with very basic support to allow running git
    commands through sudo, so a reproduction could be implemented and that
    uses only `git status` as a proxy of the issue reported.
    
    Note that because of the way sudo interacts with the system, a much
    more complete integration with the test framework will require a lot
    more work and that was therefore intentionally punted for now.
    
    The current implementation requires the execution of a special cleanup
    function which should always be kept as the last "test" or otherwise
    the standard cleanup functions will fail because they can't remove
    the root owned directories that are used.  This also means that if
    failures are found while running the specifics of the failure might
    not be kept for further debugging and if the test was interrupted, it
    will be necessary to clean the working directory manually before
    restarting by running:
    
      $ sudo rm -rf trash\ directory.t0034-root-safe-directory/
    
    The test file also uses at least one initial "setup" test that creates
    a parallel execution directory, while ignoring the repository created
    by the test framework, and special care should be taken when invoking
    commands through sudo, since the environment is otherwise independent
    from what the test framework expects.  Indeed `git status` was used
    as a proxy because it doesn't even require commits in the repository
    to work.
    
    A new SUDO prerequisite is provided that does some sanity checking
    to make sure the sudo command that will be used allows for passwordless
    execution as root and doesn't mess with git execution paths, but
    otherwise additional work will be required to ensure additional
    commands behave as expected and that will be addressed in a later patch.
    
    Most of those characteristics make this test mostly suitable only for
    CI, but it could be executed locally if special care is taken to provide
    for some of them in the local configuration and maybe making use of the
    sudo credential cache by first invoking sudo, entering your password if
    needed, and then invoking the test by doing:
    
      $ IKNOWWHATIAMDOING=YES ./t0034-root-safe-directory.sh
    
    Reported-by: SZEDER Gábor <szeder.dev@gmail.com>
    Signed-off-by: Carlo Marcelo Arenas Belón <carenas@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/t0034-root-safe-directory.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+test_description='verify safe.directory checks while running as root'
+
+. ./test-lib.sh
+
+if [ "$IKNOWWHATIAMDOING" != "YES" ]
+then
+	skip_all="You must set env var IKNOWWHATIAMDOING=YES in order to run this test"
+	test_done
+fi
+
+# this prerequisite should be added to all the tests, it not only prevents
+# the test from failing but also warms up any authentication cache sudo
+# might need to avoid asking for a password
+test_lazy_prereq SUDO '
+	sudo -n id -u >u &&
+	id -u root >r &&
+	test_cmp u r &&
+	command -v git >u &&
+	sudo command -v git >r &&
+	test_cmp u r
+'
+
+test_expect_success SUDO 'setup' '
+	sudo rm -rf root &&
+	mkdir -p root/r &&
+	sudo chown root root &&
+	(
+		cd root/r &&
+		git init
+	)
+'
+
+test_expect_failure SUDO 'sudo git status as original owner' '
+	(
+		cd root/r &&
+		git status &&
+		sudo git status
+	)
+'
+
+# this MUST be always the last test, if used more than once, the next
+# test should do a full setup again.
+test_expect_success SUDO 'cleanup' '
+	sudo rm -rf root
+'
+
+test_done
